5e4b33ef0f43b76ddf3429eefc070f2a232e2470
nik zaim <zaim_shark@yahoo.com>
2019-01-04 01:31:30 +0800

    commit again

---
+++ MVC project dotnet/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MVC_project_dotnet.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("TSNUsers");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("TSNUserClaims");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("TSNUserLogins");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("TSNUserRoles");
+            modelBuilder.Entity<IdentityRole>().ToTable("TSNRoles");
+        }
+    }
+}
